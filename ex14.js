c9b8b2656f71324005dcad9140aecc6dff6aa1a3
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:32:33 +0500

    Add files via upload

---
+++ ex14.js
@@ -0,0 +1,15 @@
+"use strict";
+// Q: 14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let GuestList = [
+    "Abdul Razzaq",
+    "Lubna",
+    "Unzila",
+    "Arsala",
+    "Feroze Khan",
+    "Zain Imam",
+    "Andrew tate",
+    "Candra",
+];
+GuestList.forEach((Guestname) => {
+    console.log(`Dear,${Guestname} you are invited to have dinner with us tonight.`);
+});
---
+++ ex14.ts
@@ -0,0 +1,18 @@
+// Q: 14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let GuestList : Array<string>=[
+"Abdul Razzaq",
+"Lubna",
+"Unzila",
+"Arsala",
+"Feroze Khan",
+"Zain Imam",
+"Andrew tate",
+"Candra",
+];
+GuestList.forEach((Guestname) =>{
+    console.log(`Dear,${Guestname} you are invited to have dinner with us tonight.`)
+
+})
+
+    
